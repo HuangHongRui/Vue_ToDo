0f7d23febfad80a1e9d5bafa5415167f2185032b
Rui <464362353@qq.com>
2017-07-25 14:34:38 +0800

    RENAME

---
+++ Demo_2_VueStart/app.js
@@ -0,0 +1,16 @@
+//import bar from './bar';
+
+//bar();
+
+
+import bar from './bar';
+import Vue from 'vue'
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message : ' HELLO Raine >>  '
+  }
+})
+
+
